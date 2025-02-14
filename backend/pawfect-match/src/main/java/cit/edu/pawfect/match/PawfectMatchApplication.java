e7674e735d6ecad244d684b2d4018edddeaaef45
LAB23_USER ON WS44 <L23X14W44@citulab.net>
2025-02-14 14:12:12 +0800

    Added backend Spring Boot and directory structure

---
+++ backend/pawfect-match/src/main/java/cit/edu/pawfect/match/PawfectMatchApplication.java
@@ -0,0 +1,13 @@
+package cit.edu.pawfect.match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawfectMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawfectMatchApplication.class, args);
+	}
+
+}
