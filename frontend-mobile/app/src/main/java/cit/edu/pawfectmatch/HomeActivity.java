7a4c4bacb187f886f1866c1f322979fc233336bd
jannahlovelle <153150092+jannahlovelle@users.noreply.github.com>
2025-04-04 21:13:48 +0800

    Google Sign In Implementation

---
+++ frontend-mobile/app/src/main/java/cit/edu/pawfectmatch/HomeActivity.java
@@ -0,0 +1,64 @@
+package cit.edu.pawfectmatch;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class HomeActivity extends AppCompatActivity {
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    TextView name, email;
+    Button signOutBtn;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_homescreen);
+        name = findViewById(R.id.home_username);
+        email = findViewById(R.id.home_email);
+        signOutBtn = findViewById(R.id.home_signOutBtn);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct!=null){
+            String personName = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+            name.setText(personName);
+            email.setText(personEmail);
+        }
+
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+    }
+
+    void signOut(){
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
--- frontend-mobile/app/src/main/java/cit/edu/pawfectmatch/LoginActivity.java
+++ frontend-mobile/app/src/main/java/cit/edu/pawfectmatch/LoginActivity.java
@@ -1,26 +1,40 @@
 package cit.edu.pawfectmatch;
 
-import java.util.logging.Handler;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class LoginActivity extends AppCompatActivity {
     private int failedAttempts = 0;
     private static final int MAX_ATTEMPTS = 5;
-    private static final int LOCK_TIME = 15000;
     private EditText email, password;
     private TextView signupText;
     private Button loginButton;
-    private Handler handler = new Handler();
+    ImageView googleBtn;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +47,14 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
 
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
         loginButton = findViewById(R.id.loginButton);
         signupText = findViewById(R.id.signupLinkText);
+        googleBtn = findViewById(R.id.google);
 
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,15 +73,6 @@ public class LoginActivity extends AppCompatActivity {
                         loginButton.setEnabled(false);
                         Toast.makeText(LoginActivity.this, "Too many failed attempts! Try again in 15 seconds.", Toast.LENGTH_LONG).show();
 
-                        // Re-enable the button after 15 seconds
-                        handler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                loginButton.setEnabled(true);
-                                failedAttempts = 0; // Reset failed attempts after cooldown
-                            }
-                        }, LOCK_TIME);
-
                     }
                 }
             }
@@ -73,11 +81,42 @@ public class LoginActivity extends AppCompatActivity {
         signupText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-                startActivity(intent);
+                Intent signUpIntent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(signUpIntent);
                 overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
             }
         });
 
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    void signIn(){
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                navigateToHomeActivity();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(),"Something went wrong",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    void navigateToHomeActivity(){
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
